60a0fa599b3d57bfe7ff9c0b1c8a2bb3cfd7fb9c
farisbizar <79406890+farisbizar@users.noreply.github.com>
2021-03-09 15:10:56 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,92 @@
+<html>
+    <head>
+    <title>CURRICULUM VITAE</title>
+
+    </head>
+    <body>
+    <h1 align=center>CURRICULUM VITAE</h1>
+    <!--INFORMASI DATA DIRI-->
+        <hr>
+        <b>PERSONAL INFORMATION</b>
+        <hr>
+        <table style="width:50%">
+            <!--FOTO-->
+            <img src="fotoku.jpg" align="right" width="104" height="142">
+        <tr>
+        <th></th>
+        <th></th>
+        </tr>
+        <tr>
+            <td>Nama Lengkap</td>
+            <td>: Faris Muhamad Ramdan Abizar </td>
+        </tr>
+        <tr>
+            <td>Tempat Tanggal Lahir</td>
+            <td>: Sukabumi, 11 oktober 2004 </td>  
+        </tr>
+            <td>Alamat</td>
+            <td>: Jalan pelabuhan 2 No 71</td>
+        <tr>
+            <td>Status</td>
+            <td>: Mahasiswa</td>
+        </tr>
+            <tr>
+            <td>NO HP</td>
+            <td>: 08889988306</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>: farisabizar@gmail</td>
+        </tr>
+        </table>
+    <!--RIWAYAT PENDIDIKAN FORMAL-->  
+            <hr>
+            <b>PENDIDIKAN FORMAL</b>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>2019-2023</td>
+                <td>SMK Negeri 2 kota Sukabumi </td>
+            </tr>
+            <tr>
+                <td>2010-2013</td>
+                <td>SMP Negeri 3 kota Sukabumi </td>  
+            </tr>
+                <td>2004-2010</td>
+                <td>SDN Begeg 2 kota Sukabumi </td>
+            <tr>
+                <td>2003-2004</td>
+                <td>paud madrasah diniyah</td>
+            </tr>
+            </table>
+    <!--RIWAYAT PENDIDIKAN INFORMAL-->      
+                   
+    <!-- RIWAYAT ORGANISASI-->
+                            <hr>
+                            <b>ORGANISASI</b><hr>
+                            <table style="width:180%">
+                        <tr>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                        <tr>
+                            <td>2016</td>
+                            <td>Ketua SUKADANA MUSIC Crew</td>
+                        </tr>
+                        <tr>
+                            <td>2017</td>
+                            <td>Ketua pemuda </td>  
+                        </tr>
+                        </table>
+    <!--KEAHLIAN YANG DIMILIKI-->                  
+                            <hr>
+                            <b>KEAHLIAN</b><hr>
+                        Mengoperasikan Komputer : Microsoft Office<br>
+                        Desain Grafis: CorelDRAW & Adobe Ilustrator<br>
+    <a href="indeks.html">KEMBALI KE INDEKS</a><br>
+    </body>
+</html>
